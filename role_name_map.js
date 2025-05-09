0bd16b6bdccadfdc351a1f0ee6c5b77ec1f2edd1
PaulChaoyupo <cyoupaul@gmail.com>
2025-05-09 23:33:40 +0800

    Update role_name_map.js

--- role_name_map.js
+++ role_name_map.js
@@ -1,33 +1,33 @@
 const roleNameMap = {
-  "team_team": "社交橋樑者",
-  "team_execution": "團隊溝通者",
-  "team_creativity": "靈感協作家",
-  "team_workstyle": "穩定領航者",
-  "team_awareness": "共感觀察家",
+  "team_team": "社交高手",
+  "team_execution": "協作行動家",
+  "team_creativity": "創意連結者",
+  "team_workstyle": "組織管家",
+  "team_awareness": "同理觀察者",
 
-  "execution_team": "推進行動者",
-  "execution_execution": "積極實踐者",
-  "execution_creativity": "執行創意者",
-  "execution_workstyle": "堅持開拓者",
-  "execution_awareness": "行動洞察者",
+  "execution_team": "行動領袖",
+  "execution_execution": "執行王者",
+  "execution_creativity": "創新實踐者",
+  "execution_workstyle": "推進專家",
+  "execution_awareness": "堅持者",
 
-  "creativity_team": "靈感連結者",
-  "creativity_execution": "創新導向者",
-  "creativity_creativity": "靈感開發者",
-  "creativity_workstyle": "靈感行動派",
-  "creativity_awareness": "靈感覺察者",
+  "creativity_team": "點子協作家",
+  "creativity_execution": "靈感行動派",
+  "creativity_creativity": "創意策劃者",
+  "creativity_workstyle": "彈性實踐者",
+  "creativity_awareness": "洞察創想家",
 
-  "workstyle_team": "穩健合作家",
-  "workstyle_execution": "節奏掌握者",
-  "workstyle_creativity": "穩定創造者",
-  "workstyle_workstyle": "穩健執行者",
-  "workstyle_awareness": "穩定觀察者",
+  "workstyle_team": "穩定推進者",
+  "workstyle_execution": "流程掌控者",
+  "workstyle_creativity": "規劃創新者",
+  "workstyle_workstyle": "自律執行者",
+  "workstyle_awareness": "有序思考者",
 
-  "awareness_team": "洞察觀察者",
-  "awareness_execution": "反思實踐者",
-  "awareness_creativity": "深度靈感者",
-  "awareness_workstyle": "自我精進者",
-  "awareness_awareness": "自我洞察家"
+  "awareness_team": "敏感觀察者",
+  "awareness_execution": "自省實踐家",
+  "awareness_creativity": "洞察創意家",
+  "awareness_workstyle": "內省規劃者",
+  "awareness_awareness": "深思者"
 };
 
 export default roleNameMap;
